57e2d23ae31d0f5138b66b6127775dff28bf3729
rary <xogml6915@gmail.com>
2022-06-11 14:52:51 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div v-if="state.account.id">안녕하세요? {{ state.account.name }}님!</div>
+    <div v-else>
+      <label for="loginId">
+        <span>아이디</span>
+        <input type="text" id="loginId" v-model="state.form.loginId"/>
+      </label>
+      <label for="loginPw">
+        <span>비밀번호</span>
+        <input type="text" id="loginPw" v-model="state.form.loginPw"/>
+      </label>
+      <hr>
+      <button @click="submit()">로그인</button>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//npm i axios =  (서버통신)설치
+
+import axios from "axios";
+import {reactive} from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const state = reactive({
+      account: {
+        id: null,
+        name: ""
+      },
+      form: {
+        loginId: "",
+        loginPw: ""
+      },
+      loggedIn: false
+    });
+
+    const submit = () => {
+      const args = {
+        loginId: state.form.loginId,
+        loginPw: state.form.loginPw
+      }
+      axios.post("/api/account", args).then((res)=>{
+        alert("로그인에 성공했습니다.");
+        state.account = res.data;
+      });
+
+    }
+
+    /*
+    * cors err => 다른서버 통신시 그냥하면 cross domain 에러가 생긴다 (8080 => 3000)
+    * proxy사용 vue.config
+    * */
+    axios.get("/api/account").then((res) => {
+      state.account = res.data;
+    });
+
+    return {state, submit}
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //cors err 뜨면 사용한다. url 우회
+  devServer:{
+    proxy:{
+      "/api":{
+        target:"http://localhost:3000"
+      }
+    }
+  }
+
+
 })
